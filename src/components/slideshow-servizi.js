0d2186733c3697d93c1638356c908b28d5e882e1
Matteo Sacchi <matteoseik@hotmail.it>
2025-02-10 14:26:04 +0100

    Update slideshow-servizi.js

--- src/components/slideshow-servizi.js
+++ src/components/slideshow-servizi.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { PrismicRichText } from '@prismicio/react'
 import { PrismicNextImage } from '@prismicio/next';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Mousewheel, EffectFade, Autoplay } from 'swiper';
@@ -15,6 +15,7 @@ const SlideshowServizi = ({ content, index }) => {
 
   const swiperRef = useRef(null);
   const slideRef = useRef(null);
+  const [isMuted, setIsMuted] = useState(true);
 
   
   useEffect(() => {
@@ -24,27 +25,42 @@ const SlideshowServizi = ({ content, index }) => {
   }, [index]);
 
 
-  useEffect(()=>{
-
+  useEffect(() => {
     let videos = slideRef.current.querySelectorAll("video");
     videos.forEach(item => {
       item.pause();
       item.currentTime = 0;
+      item.muted = true;
     });
 
     let active = slideRef.current.querySelector(".swiper-slide-active");
-    let video = active.querySelectorAll("video");
-    if(video){
-      video.forEach(item => {
-        item.currentTime = 0;
-        item.play();
-      });
+    let activeVideo = active.querySelector("video");
+    if (activeVideo) {
+      activeVideo.play();
     }
-  }, [index])
+  }, [index]); // Rimuoviamo isMuted dalle dipendenze
 
+  // Aggiungiamo un nuovo useEffect separato per gestire solo l'audio
+  useEffect(() => {
+    let active = slideRef.current.querySelector(".swiper-slide-active");
+    let activeVideo = active.querySelector("video");
+    if (activeVideo) {
+      activeVideo.muted = isMuted;
+    }
+  }, [isMuted]); // Aggiungiamo isMuted come dipendenza
+
+  const handleToggleAudio = () => {
+    const active = slideRef.current.querySelector(".swiper-slide-active");
+    const activeVideo = active.querySelector("video");
+    if (activeVideo) {
+      activeVideo.muted = !isMuted;
+      setIsMuted(!isMuted);
+    }
+  };
 
   return(
-    <div ref={slideRef} className='h-full w-full pointer-events-none'>    
+    <div ref={slideRef} className='h-full w-full pointer-events-none'> 
+ 
           <Swiper
             ref={swiperRef}
             modules={[Navigation, A11y]}
@@ -59,7 +75,8 @@ const SlideshowServizi = ({ content, index }) => {
           {content?.map((item, i)=>{
             return(
              <SwiperSlide key={i}>
-             {item.video ? (
+             {item.video.url ? (
+                <>
                <video 
                  autoPlay 
                  muted 
@@ -70,6 +87,21 @@ const SlideshowServizi = ({ content, index }) => {
                >
                  <source src={item.video.url} type="video/mp4" />
                </video>
+               <button 
+               onClick={handleToggleAudio}
+               className='absolute bottom-2 right-2 z-10 pointer-events-auto p-1 rounded-full transition-all'
+             >
+               {isMuted ? (
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="grey" className="w-3 h-3">
+                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                 </svg>
+               ) : (
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="grey" className="w-3 h-3">
+                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                 </svg>
+               )}
+             </button>
+             </>
              ) : item.immagine ? (
                <PrismicNextImage 
                  field={item.immagine}
