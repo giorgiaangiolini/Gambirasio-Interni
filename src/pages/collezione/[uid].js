351ae132a07fe196f085365d939bbf044cd81813
Giorgia Angiolini <matteoseik@hotmail.it>
2025-03-20 09:47:12 +0100

    t

--- src/pages/collezione/[uid].js
+++ src/pages/collezione/[uid].js
@@ -12,9 +12,9 @@ export default function Oggetto({ page, settings, locales }) {
     <Layout settings={settings} meta={data} altLangs={locales}>
       <div className="flex md:flex-row flex-col min-h-full md:pt-5 pt-6 md:pb-2 pb-4 md:px-4 px-1">
         <div className="md:w-[30%] w-full">
-          <div className="sticky flex flex-col gap-2 md:top-[50vh] md:-translate-y-1/2 md:pr-8 text-grey md:text-base text-xs md:mb-0 mb-1">
+          <div className="sticky flex flex-col md:gap-2 gap-1 md:top-[50vh] md:-translate-y-1/2 md:pr-8 text-grey md:text-base text-xs md:mb-0 mb-1">
             <FadeInAnimation>{data.descrizione}</FadeInAnimation>
-            <a href="mailto:info@gambirasio.it" target="_blank" className="text-blue underline hover:opacity-60">Richiedi informazioni</a>
+            <a href="mailto:info@gambirasio.it" target="_blank" className="text-grey underline hover:opacity-60">Richiedi informazioni</a>
           </div>
         </div>
         <div className="md:w-[70%] w-full">
